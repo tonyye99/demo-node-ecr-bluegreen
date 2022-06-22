25dc083aece01002026a0afc89a430fcf2b2c3ac
Ye Htet Aung <ygxan18@gmail.com>
2022-06-22 12:07:52 +0630

    latest

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("ECS bluegreen codedeploy with github workflows");
+  res.send("ECS bluegreen codedeploy with github workflows. Latest changes !!!!!");
 });
 
 app.listen(port, () => {
